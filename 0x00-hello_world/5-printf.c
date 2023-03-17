3acf26828805564a90d17524172e24e856689060
Nomso123 <chinomsoanyanwu227@gmail.com>
2023-03-16 21:57:55 -0700

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 int main(void){
-printf("with proper grammer,but the outcome is a piece of art\n");
+printf("with proper grammer , but the outcome is a peice of art , 
 return (0);
 }
