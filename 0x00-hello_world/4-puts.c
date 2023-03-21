0eaa708536bcfb9afad96b9a9f206dd9acc92378
Nomso123 <chinomsoanyanwu227@gmail.com>
2023-03-20 20:41:08 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main(void){
-puts("Programming is like building a multilingual puzzle\n");
+/**
+ * main - A c programme to print
+ * Return : 0 (Sucess)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
