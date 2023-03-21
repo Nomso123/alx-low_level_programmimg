68400189e644cb506134cd178e9b9c43e8acf596
Nomso123 <chinomsoanyanwu227@gmail.com>
2023-03-20 21:38:58 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,4 @@
 #include <stdio.h>
 /**
- * main - A c programme to print
- * Return : 0 (Sucess)
- */
-int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}
+  * Return: Always 0.
+ * Return: Always return 0.
