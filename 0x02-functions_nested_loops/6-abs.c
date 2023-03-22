3e6c3c2c73c0e487159589a9adb9078195669a5c
Nomso123 <chinomsoanyanwu227@gmail.com>
2023-03-21 18:39:24 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - primts  abbsolute
+ * @c: parameter
+ *
+ * Return: returns abs
+ */
+int _abs(int c)
+{
+int abs_val;
+abs_val = c * -1;
+return (abs_val);
+}
+return (c);
+}
