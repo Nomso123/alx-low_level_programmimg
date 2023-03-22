561d89159eddd68797a5a13c0161592e76e39679
Nomso123 <chinomsoanyanwu227@gmail.com>
2023-03-21 20:30:53 -0700

    add

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,4 +6,7 @@ int print_last_digit(int);
 void jack_bauer(void);
 void print_alphabet_x10(void);
 int _putchar(char c);
+int add(int, int);
+void times_table(void);
+void print_to_98(int n);
 void print_alphabet(void);
