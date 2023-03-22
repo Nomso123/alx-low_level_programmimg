76913af1d19bb777bcfbe282009c506f5ad4666b
Nomso123 <chinomsoanyanwu227@gmail.com>
2023-03-21 19:26:35 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - prints sign of number
+ * @n: parameter
+ *
+ * Return: returns sign
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar(43);
+return (1);
+}
+else if (n < 0)
+{
+_putchar(45);
+return (-1);
+}
+else
+{
+_putchar(40);
+return (0);
+}
+}
