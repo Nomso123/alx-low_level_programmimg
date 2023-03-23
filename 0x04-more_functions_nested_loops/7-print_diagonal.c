bc0449ebeffaf446959e478d31042597d676afce
Nomso123 <chinomsoanyanwu227@gmail.com>
2023-03-23 16:44:59 -0700

    7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,8 +16,10 @@ for (i = 0; i < n; i++)
 {
 for (j = 0; j < n; j++)
 {
-if (j == i)                                   _putchar('\\');
-else if (j < i)                                   _putchar(' ');
+if (j == i)
+_putchar('\\');
+else if (j < i)
+_putchar(' ');
 }
 _putchar('\n');
 }
